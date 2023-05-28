9113fe7ba437a699461d702cfd36527a6ee65103
Andriikryl <andreyk557@gmail.com>
2023-05-28 23:55:54 +0300

    feat[add -second - type of accordeon]

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-import './style.css'
+import "./style.css";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const accordions = document.querySelectorAll(".accordion");
+
+  accordions.forEach((el) => {
+    el.addEventListener("click", (e) => {
+      const self = e.currentTarget;
+      const controls = self.querySelector(".accordion__control-two");
+      const content = self.querySelector(".accordion__content-two");
+
+      content.classList.toggle("open");
+    });
+  });
+});
